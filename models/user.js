755baac18426057784b98006bb8739010e7050e5
Austin Gil <austin@stegosource.com>
2017-10-06 08:17:11 -0700

    Hashing user passwords with bcrypt before saving

--- models/user.js
+++ models/user.js
@@ -13,3 +13,14 @@ const userSchema = new mongoose.Schema({
 		required: true
 	}
 })
+
+userSchema.pre('save', async function (next) {
+	try {
+		const salt = await bcrypt.genSalt(10);
+		const passwordHash = await bcrypt.hash(this.password, salt);
+		this.password = passwordHash;
+		next();
+	} catch (err) {
+
+	}
+})
