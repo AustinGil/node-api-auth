1ac3035cb3c63cd344874cb58a59b43d73a288b5
Austin Gil <austin@stegosource.com>
2017-10-06 08:13:36 -0700

    Adding initial User model

---
+++ models/user.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+
+const userSchema = new mongoose.Schema({
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+		lowercase: true
+	},
+	password: {
+		type: String,
+		required: true
+	}
+})
